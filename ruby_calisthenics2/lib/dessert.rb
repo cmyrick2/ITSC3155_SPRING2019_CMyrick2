ec786fc818ad246d8f2abe522b8d60d64ebcf7aa
cmyrick2 <cmyrick2@uncc.edu>
2019-09-20 02:04:34 +0000

    Ruby OOP - Part 3

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -23,7 +23,11 @@ end
 
 class JellyBean < Dessert
   # add code for setters and getters
+  attr_accessor :flavor
   def initialize(flavor)
     # your code here
+    @name = "#{flavor} jelly bean"
+    @calories = 5
+    @flavor   = flavor
   end
 end
